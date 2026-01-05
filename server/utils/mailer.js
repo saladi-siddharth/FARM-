35e50235fc9095e6b2c57549b90480df2782ea79
saladi-siddharth <saladipranathi@gmail.com>
2026-01-05 06:58:25 +0530

    Force IPv4 for Nodemailer to fix Render connection timeouts

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -13,9 +13,10 @@ const createTransporter = () => {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
         },
-        connectionTimeout: 10000, // 10 seconds
+        connectionTimeout: 10000,
         greetingTimeout: 10000,
         socketTimeout: 10000,
+        family: 4, // <--- FORCE IPv4 (Fixes Render/Gmail timeouts)
         tls: {
             rejectUnauthorized: false
         }
